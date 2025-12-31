b43061499d74caef2284df411bf8732ddd05cdca
SergioCostaczr <manoelsergio.dev@gmail.com>
2025-12-31 18:01:22 -0300

    Project created

---
+++ src/main/java/com/educandoweb/projetosb/ProjetosbApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projetosb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/projetosb/ProjetosbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.projetosb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
