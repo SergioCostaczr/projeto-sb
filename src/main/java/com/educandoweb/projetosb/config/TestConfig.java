72d4306c151b6b3f96551d56e8388782de6e86d6
SergioCostaczr <manoelsergio.dev@gmail.com>
2026-01-01 16:58:40 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/educandoweb/projetosb/config/TestConfig.java
+++ src/main/java/com/educandoweb/projetosb/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.educandoweb.projetosb.config;
 
-import com.educandoweb.projetosb.entities.Category;
-import com.educandoweb.projetosb.entities.Order;
-import com.educandoweb.projetosb.entities.Product;
-import com.educandoweb.projetosb.entities.User;
+import com.educandoweb.projetosb.entities.*;
 import com.educandoweb.projetosb.entities.enums.OrderStatus;
-import com.educandoweb.projetosb.respositories.CategoryRepository;
-import com.educandoweb.projetosb.respositories.OrderRepository;
-import com.educandoweb.projetosb.respositories.ProductRepository;
-import com.educandoweb.projetosb.respositories.UserRepository;
+import com.educandoweb.projetosb.respositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -34,6 +28,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
     @Override
     public void run(String... args) throws Exception {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
@@ -44,6 +41,8 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+
+
 
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
@@ -67,6 +66,11 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
+        orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
     }
 }
